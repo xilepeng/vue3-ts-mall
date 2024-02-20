ff9781d87ae4cd24290c56c8db56b4a0461d79ad
X <lepengxi@163.com>
2024-02-20 14:53:16 +0800

    7 登录页

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { lazyPlugin } from './directives'
+
 // 引入全局组件插件
 import { componentPlugin } from '@/components'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 
-app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
